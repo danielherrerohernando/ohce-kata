f232f219a60c4452c32888b9bb07ca5331524de1
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-09 16:29:18 +0100

    test: adding tests for time-dependent greeting
    update for greetings unit tests
    
    danielherrerohernando/ohce-kata#3

--- test/unit/greetings.test.js
+++ test/unit/greetings.test.js
@@ -2,8 +2,16 @@ const { expect } = require('chai');
 const { greetUser } = require('../../src/greetings');
 
 describe('Unit test - Greetings Handler', () => {
-	it('Should greet user including the username', () => {
-		const greeting = greetUser('Daniel');
+	it('Should greet user including the username and depending on current time - Morning', () => {
+		const greeting = greetUser('Daniel', 8);
 		expect(greeting).to.equal('¡Buenos días Daniel!');
+	});
+	it('Should greet user including the username and depending on current time - Afternoon', () => {
+		const greeting = greetUser('Daniel', 15);
+		expect(greeting).to.equal('¡Buenas tardes Daniel!');
+	});
+	it('Should greet user including the username and depending on current time - Night', () => {
+		const greeting = greetUser('Daniel', 23);
+		expect(greeting).to.equal('¡Buenas noches Daniel!');
 	});
 });
