16e068df6f595dc848c4550fd145ff920a66be7b
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-09 23:50:49 +0100

    test: enabling ipc for mocking in integration testing
    update for the test helper to allow mocking in the child process
    
    danielherrerohernando/ohce-kata#4

--- test/testHelper/testHelper.js
+++ test/testHelper/testHelper.js
@@ -2,13 +2,14 @@ const { spawn } = require('child_process');
 const concat = require('concat-stream');
 
 const execute = processPath => userName => {
-	const childProcess = spawn('node', [processPath, userName]);
+	const childProcess = spawn('node', [processPath, userName], { stdio: [null, null, null, 'ipc'] });
 	const promise = new Promise((resolve, reject) => {
 		childProcess.stdout.setEncoding('utf-8');
 		childProcess.on('error', reject);
 		childProcess.stderr.once('data', err => reject(err));
 		childProcess.stdout.pipe(concat(result => resolve(result)));
 	});
+	promise.relatedProcess = childProcess;
 	return promise;
 };
 
