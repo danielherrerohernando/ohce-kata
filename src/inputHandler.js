160f47612c5e099f7be6edcb0c61a8f691a88e2e
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-10 12:44:28 +0100

    test: adding test for reversing input function
    unit test added
    
    danielherrerohernando/ohce-kata#3

---
+++ src/inputHandler.js
@@ -0,0 +1,3 @@
+const reverseInput = () => {};
+
+module.exports = { reverseInput };
--- test/unit/inputHandler.test.js
+++ test/unit/inputHandler.test.js
@@ -1,3 +1,8 @@
+const { expect } = require('chai');
+const { reverseInput } = require('../../src/inputHandler');
+
 describe('Unit test - Input Handler', () => {
-	it('Should do sth', () => {});
+	it('Should reverse input', () => {
+		expect('epilef').to.equal(reverseInput('felipe'));
+	});
 });
