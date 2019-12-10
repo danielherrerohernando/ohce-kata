8b9024d449cfd62cc99bf0416552c0192ea670bf
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-10 13:05:08 +0100

    test: adding e2e test with inputs
    adding test for reversing input and check stop call
    
    danielherrerohernando/ohce-kata#4

--- test/integration-e2e/ohceCliApp.test.js
+++ test/integration-e2e/ohceCliApp.test.js
@@ -8,7 +8,7 @@ describe('Integration-E2E Tests', () => {
 	before(() => {
 		basicProcess = testHelper.execute('./src/index.js');
 	});
-	describe('The ohce cli app', () => {
+	describe('The ohce cli app basic greetings', () => {
 		it('Should greet user on startup - Morning', async () => {
 			const promiseFromChildProcess = basicProcess('Daniel');
 			const childProcess = promiseFromChildProcess.relatedProcess;
@@ -32,6 +32,16 @@ describe('Integration-E2E Tests', () => {
 			childProcess.send({ start: 'start' });
 			const output = await promiseFromChildProcess;
 			expect(output).to.startsWith('¡Buenas noches Daniel!');
+		});
+	});
+	describe('The ohce cli app handles input', () => {
+		it('Should greet user on startup - Morning & Reverse some input & Handle stop', async () => {
+			const promiseFromChildProcess = basicProcess('Daniel', ['lightning', 'stop']);
+			const childProcess = promiseFromChildProcess.relatedProcess;
+			childProcess.send({ mockTime: mockTimeMorning });
+			childProcess.send({ start: 'start' });
+			const output = await promiseFromChildProcess;
+			expect(output).to.equal('¡Buenos días Daniel!\ngninthgil\npots\nAdios Daniel');
 		});
 	});
 });
