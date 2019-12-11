5b5c8e0c95c5569f245e31c2ea87a1029147d3f5
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2019-12-12 00:00:36 +0100

    Currify the greet function

--- src/greetings.js
+++ src/greetings.js
@@ -3,7 +3,7 @@ const { GOODMORNING, GOODAFTERNOON, GOODNIGHT, BYE } = require('./constants');
 const isMorning = hour => hour >= 6 && hour < 12;
 const isAfternoon = hour => hour >= 12 && hour < 20;
 
-const greetUser = (username, hour = 8) => {
+const greetUser = username => (hour = 8) => {
 	return isMorning(hour) ? `¡${GOODMORNING} ${username}!` :
 		isAfternoon(hour) ? `¡${GOODAFTERNOON} ${username}!` :
 			`¡${GOODNIGHT} ${username}!`;
