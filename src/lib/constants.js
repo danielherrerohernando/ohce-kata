9d471155cc3e3bb47004962e2d5bbfda50af055b
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-11 02:20:45 +0100

    test: adding test for enhanced palindromechecker
    tests for checking palindromes in sentences too
    
    danielherrerohernando/ohce-kata#3

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,6 +3,7 @@ const GOODAFTERNOON = 'Buenas tardes';
 const GOODNIGHT = 'Buenas noches';
 const BYE = 'Adios';
 const NICEWORD = '¡Bonita palabra!';
+const NICESENTENCE = '¡Bonita frase!';
 const STOP = 'Stop!';
 
-module.exports = { GOODMORNING, GOODAFTERNOON, GOODNIGHT, BYE, NICEWORD, STOP };
+module.exports = { GOODMORNING, GOODAFTERNOON, GOODNIGHT, BYE, NICEWORD, NICESENTENCE, STOP };
--- test/unit/inputHandler.test.js
+++ test/unit/inputHandler.test.js
@@ -11,4 +11,15 @@ describe('Unit test - Input Handler', () => {
 	it('Should detect palindromes and return null when it is not a palindrome', () => {
 		expect(null).to.equal(palindromeChecker('guidesmiths', 'shtimsediug'));
 	});
+	it('Should detect palindromes even when they are sentences with commas and spaces (I)', () => {
+		expect('¡Bonita frase!').to.equal(palindromeChecker('race car', 'rac ecar'));
+	});
+	it('Should detect palindromes even when they are sentences with commas and spaces (II)', () => {
+		expect('¡Bonita frase!').to.equal(
+			palindromeChecker('A man, a plan, a canal. Panama', 'amanaP .lanac a ,nalp a ,nam A')
+		);
+	});
+	it('Should detect palindromes even when they are sentences with commas and spaces, or return null when it is not', () => {
+		expect(null).to.equal(palindromeChecker('A man, a ham', 'mah a ,nam A'));
+	});
 });
